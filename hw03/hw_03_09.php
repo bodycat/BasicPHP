72ca65a4818a6eaa67348a5f8975b1bebee102b9
Khizri Tataev <bodycat@gmail.com>
2019-05-07 19:09:06 +0300

    Add files via upload

---
+++ hw03/hw_03_09.php
@@ -0,0 +1,67 @@
+<?php
+// *Объединить две ранее написанные функции в одну, которая получает строку на русском языке, производит транслитерацию и замену пробелов на подчеркивания (аналогичная задача решается при конструировании url-адресов на основе названия статьи в блогах).
+header("Content-type:text/html; charset=utf-8");
+
+$string = "<p>Съешь ещё этих мягких французских булок, да выпей [же] чаю. 1234567890.
+Из «Виндоуса-95» (fontview.exe)</p>";
+
+function translit($string)
+{
+    $dictionary = [
+        " " => "_",
+        "а" => "a",
+        "б" => "b",
+        "в" => "v",
+        "г" => "g",
+        "д" => "d",
+        "е" => "e",
+        "ё" => "e",
+        "ж" => "zh",
+        "з" => "z",
+        "и" => "i",
+        "й" => "y",
+        "к" => "k",
+        "л" => "l",
+        "м" => "m",
+        "н" => "n",
+        "о" => "o",
+        "п" => "p",
+        "р" => "r",
+        "с" => "s",
+        "т" => "t",
+        "у" => "u",
+        "ф" => "f",
+        "х" => "h",
+        "ц" => "c",
+        "ч" => "ch",
+        "ш" => "sh",
+        "щ" => "sch",
+        "ь" => "'",
+        "ы" => "y",
+        "ъ" => "''",
+        "э" => "e",
+        "ю" => "yu",
+        "я" => "ya"
+    ];
+
+// Преобразование строки в массив. Решение проблемы с UTF-8
+// https://stackoverflow.com/questions/21653033/php-how-to-split-a-utf-8-string
+    $stringToArray = preg_split('//u', $string, -1, PREG_SPLIT_NO_EMPTY);
+
+    foreach ($stringToArray as $key => $character) {
+        foreach ($dictionary as $rus => $trans) {
+            if ($character == $rus) {
+                $stringToArray[$key] = $trans;
+                break;
+            } elseif ($character == mb_strtoupper($rus)) {
+                $stringToArray[$key] = mb_strtoupper($trans);
+                break;
+            }
+        }
+    }
+
+// implode — Объединяет элементы массива в строку
+    return implode($stringToArray);
+}
+echo "<b>Исходный текст:</b> $string <hr>";
+echo "<b>Текст с транслитерацией:</b>" . translit($string);
